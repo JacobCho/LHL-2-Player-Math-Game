16413a30832ca0863643b5acd69a43c1c86f66fa
Jacob Cho <jacobshcho@gmail.com>
2014-10-07 15:27:57 -0700

    Player turn function added

--- 2-Player Math Game/main.c
+++ 2-Player Math Game/main.c
@@ -12,38 +12,50 @@
 
 struct Player {
     
+    char *name;
     int lives;
     int score;
 };
 
+enum {
+    addition,
+    subtraction,
+    multiplication,
+    division
+} Operator;
+
 typedef struct Player Player;
 
-int randNum();
+int randNum(int n);
 bool hasLives(Player a, Player b);
-bool checkAnswer(int num1, int num2, int answer);
+bool checkAnswer(int num1, int operator, int num2, int answer);
+void playerTurn(Player player, int num1, int operator, int num2);
 
 int main(int argc, const char * argv[]) {
 
     // Initialize Player A
     Player playerA;
     playerA.lives = 3;
+    playerA.name = "Player 1";
     
     // Initialize Player B
     Player playerB;
     playerB.lives = 3;
+    playerB.name = "Player 2";
     
     while (hasLives(playerA, playerB)) {
         
-        int num1 = randNum();
-        int num2 = randNum();
+        int num1 = randNum(21);
+        int num2 = randNum(21);
+        int operator = randNum(4);
         char answer[10];
         int answerNum;
-        
+   
         // Player 1 takes a turn
-        printf("Player 1: What does %d plus %d equal?\n", num1, num2);
+        playerTurn(playerA, num1, operator, num2);
         fgets(answer,10,stdin);
         answerNum = atoi(answer);
-        if (checkAnswer(num1, num2, answerNum)) {
+        if (checkAnswer(num1, operator, num2, answerNum)) {
             playerA.score++;
             printf("Correct! Player 1 score is now: %d\n",playerA.score);
         }
@@ -53,12 +65,13 @@ int main(int argc, const char * argv[]) {
         }
         
         // Player 2 takes a turn
-        num1 = randNum();
-        num2 = randNum();
-        printf("Player 2: What does %d plus %d equal?\n", num1, num2);
+        num1 = randNum(21);
+        num2 = randNum(21);
+        operator = randNum(4);
+        playerTurn(playerB, num1, operator, num2);
         fgets(answer,10,stdin);
         answerNum = atoi(answer);
-        if (checkAnswer(num1, num2, answerNum)) {
+        if (checkAnswer(num1, operator, num2, answerNum)) {
             playerB.score++;
             printf("Correct! Player 2 score is now: %d\n",playerA.score);
         }
@@ -67,7 +80,6 @@ int main(int argc, const char * argv[]) {
             printf("Wrong! Player 2 lives is now: %d\n",playerA.lives);
         }
 
-        
     }
     
     printf("Game Over!\n");
@@ -75,9 +87,9 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-int randNum() {
+int randNum(int n) {
     
-    int r = (int)(arc4random()% 21);
+    int r = (int)(arc4random() % n);
     
     return r;
 }
@@ -99,14 +111,60 @@ bool hasLives(Player a, Player b) {
     return true;
 }
 
-bool checkAnswer(int num1, int num2, int answer) {
+bool checkAnswer(int num1, int operator, int num2, int answer) {
     
-    int sum = num1 + num2;
+    int sum;
     
-    if (answer == sum) {
-        return true;
+    switch (operator) {
+        case addition:
+            sum = num1 + num2;
+            if (answer == sum) {
+                return true;
+            }
+            break;
+        case subtraction:
+            sum = num1 - num2;
+            if (answer == sum) {
+                return true;
+            }
+            break;
+        case multiplication:
+            sum = num1 * num2;
+            if (answer == sum) {
+                return true;
+            }
+            break;
+        case division:
+            if (num2 == 0) num2 = randNum(21);
+            sum = num1 / num2;
+            if (answer == sum) {
+                return true;
+            }
+            break;
+        default:
+            return false;
+            break;
     }
-    
     return false;
+}
+
+void playerTurn(Player player, int num1, int operator, int num2) {
     
+    switch (operator) {
+        case addition:
+            printf("%s: What does %d + %d equal?\n", player.name, num1, num2);
+            break;
+        case subtraction:
+            printf("%s: What does %d - %d equal?\n", player.name, num1, num2);
+            break;
+        case multiplication:
+            printf("%s: What does %d * %d equal?\n", player.name, num1, num2);
+            break;
+        case division:
+            printf("%s: What does %d / %d equal?\n", player.name, num1, num2);
+            break;
+        default:
+            break;
+    }
+
 }
