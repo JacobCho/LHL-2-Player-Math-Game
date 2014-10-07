c088b8fcc9bd3ae7b14b7caad42cf717e8614483
Jacob Cho <jacobshcho@gmail.com>
2014-10-07 13:38:42 -0700

    Initial commit

---
+++ 2-Player Math Game/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2-Player Math Game
+//
+//  Created by Jacob Cho on 2014-10-07.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
